73e3902e5e61c6a10726d86e2867a4346ec0b7f0
Cyril Plisko <cyril.plisko@mountall.com>
2016-08-04 18:17:55 +0300

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+//! Does all the magic to have you potentially long output piped through the
+//! external pager. Similar to what git does for its output.
+//!
+//! Quick Start
+//!
+//! ```
+//! extern crate pager;
+//!
+//! fn main() {
+//!     Pager::new().setup();
+//!     // The rest of your program goes here
+//! }
+//! ```
+//!
+//! Under the hood this forks the current process, connects child' stdout
+//! to parent's stdin, and then replaces the parent with the pager of choice
+//! (environment variable PAGER). The child just continues as normal.
+//!
+//! You can control pager to a limited degree. For example you can change the
+//! environment variable used for finding pager executable.
+//!
+//! ```
+//! extern crate pager;
+//!
+//! fn main() {
+//!     Pager::new().env("MY_PAGER").setup();
+//!     // The rest of your program goes here
+//! }
+//! ```
+//!
+//! If no PAGER is found `setup()` does nothing and your executable keeps
+//! running as usual. `Pager` cleans after itself and doesn't leak resources in
+//! case of setup failure.
+//!
+
 extern crate libc;
 
 use std::env;
