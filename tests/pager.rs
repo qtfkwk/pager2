991f1b099b7d005385b9f3004ea4b5b2def44286
Cyril Plisko <cyril.plisko@replixio.com>
2017-09-02 15:28:43 +0300

    Add integration tests

---
+++ tests/pager.rs
@@ -0,0 +1,14 @@
+extern crate pager;
+
+use std::env;
+
+use pager::Pager;
+
+#[test]
+fn nopager() {
+    env::set_var("NOPAGER", "");
+    let mut pager = Pager::new();
+    pager.setup();
+    env::remove_var("NOPAGER");
+    assert!(!pager.is_on());
+}
