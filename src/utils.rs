aced6ec635913fd6918e782f1f3b6f92aa73cf22
Dmitry Mottl <dmitry.mottl@gmail.com>
2024-06-28 17:56:30 +0700

    Refactoring of a pager execution

--- src/utils.rs
+++ src/utils.rs
@@ -1,40 +1,5 @@
-use std::ffi::{CString, OsString};
-use std::os::unix::ffi::{OsStrExt, OsStringExt};
-use std::ptr;
-
-fn osstring2cstring(s: OsString) -> CString {
-    unsafe { CString::from_vec_unchecked(s.into_vec()) }
-}
-
-fn split_string(s: &OsString) -> Vec<OsString> {
-    match s.clone().into_string() {
-        Ok(cmd) => cmd.split_whitespace().map(OsString::from).collect(),
-        Err(cmd) => vec![cmd],
-    }
-}
-
-// Helper wrappers around libc::* API
 pub(crate) fn fork() -> libc::pid_t {
     unsafe { libc::fork() }
-}
-
-pub(crate) fn execvpe(cmd: &OsString, envs: &[OsString]) {
-    let cstrings = split_string(cmd)
-        .into_iter()
-        .map(osstring2cstring)
-        .collect::<Vec<_>>();
-    let args = cstrings
-        .iter()
-        .map(|c| c.as_ptr())
-        .chain(Some(ptr::null()))
-        .collect::<Vec<_>>();
-
-    for env in envs {
-        unsafe { libc::putenv(env.as_bytes().as_ptr() as *mut _) };
-    }
-
-    errno::set_errno(errno::Errno(0));
-    unsafe { libc::execvp(args[0], args.as_ptr()) };
 }
 
 pub(crate) fn dup2(fd1: i32, fd2: i32) {
